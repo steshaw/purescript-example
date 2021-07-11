5b4da4aed978e813d6c887a5970442ae50e87acf
Steven Shaw <steven@steshaw.org>
2021-07-11 12:39:02 +1000

    HeytingAlgebra vs Monoid.Conj

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,10 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Monoid.Conj
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  log $ show $ (_ > 1) && (_ < 10) $ 3
+  log $ show $ (Conj <$> (_ > 1)) <> (Conj <$> (_ < 10)) $ 3
+  log $ show $ (\c -> case c of Conj a -> a) $ (Conj <$> (_ > 1)) <> (Conj <$> (_ < 10)) $ 3
